1731acfa06dc382cd8f57688d9393d9b48b705f3
Ivan Junckes Filho <ivanjunckes@gmail.com>
2018-07-04 14:11:43 -0300

    new version

---
+++ scripts/call_microservices.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+HOST="localhost"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    HOST="host.docker.internal"
+fi
+
+
+TOTAL_TIME=0
+for i in {1..6}
+do
+	curl -u bob:wrongpass http://$HOST:8080/movies-microservice
+	curl -u bob:12345678 http://$HOST:8080/movies-microservice
+	curl -u matt:12345678 http://$HOST:8080/movies-microservice
+	curl -u julian:12345678 http://$HOST:8080/movies-microservice
+	curl -u john:12345678 http://$HOST:8080/movies-microservice
+	curl -u steve:12345678 http://$HOST:8080/movies-microservice
+	curl -u robert:12345678 http://$HOST:8080/movies-microservice
+	scripts/sign.sh --key business-partner-key --secret "secret" GET http://$HOST:8080/movies-microservice2
+	scripts/sign.sh --key business-partner-key --secret "secret" GET http://$HOST:8080/movies-microservice3
+	scripts/sign.sh --key business-partner-key --secret "wrong_secret" GET http://$HOST:8080/movies-microservice2
+	scripts/sign.sh --key cinema-app --secret "secret" GET http://$HOST:8080/movies-microservice
+	scripts/sign.sh --key business-partner-key --secret "secret" GET http://$HOST:8080/movies-microservice
+	echo ""
+	echo "Please wait, it will be done in $((60 - TOTAL_TIME)) seconds..."
+	sleep 10s
+	TOTAL_TIME=$((TOTAL_TIME + 10))
+done
+echo "Script done."
---
+++ scripts/load_configuration.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#set -x
+
+cd "$(dirname "$0")"
+source ./import.sh
+
+
+HOST="localhost"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    HOST="host.docker.internal"
+fi
+
+# Accounts
+import http://${HOST}:8080/tag data/account-business-partner.json
+import http://${HOST}:8080/tag data/account-bob.json
+import http://${HOST}:8080/tag data/account-bob-mobile-client.json
+import http://${HOST}:8080/tag data/account-matt.json
+import http://${HOST}:8080/tag data/account-julian.json
+import http://${HOST}:8080/tag data/account-john.json
+import http://${HOST}:8080/tag data/account-steve.json
+
+
+#Routes
+import http://${HOST}:8080/tag data/google_route.json
+import http://${HOST}:8080/tag data/movies_route.json
+import http://${HOST}:8080/tag data/movies2_route.json
+
+# Account Policies
+import http://${HOST}:8080/tag data/account-policies.json
